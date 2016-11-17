294d8d218065a6eda78b4a92f5d55794c56b0d97
khas91 <khasquakhas@gmail.com>
2016-11-17 14:20:30 -0500

    Added a section to create the aggregate statistics by value

--- DataProfiler/DataElementProfiler.cs
+++ DataProfiler/DataElementProfiler.cs
@@ -16,13 +16,20 @@ namespace DataProfiler
             SqlDataReader reader;
             StreamReader file = new StreamReader("..\\..\\..\\EnumerableValues.csv");
             StreamWriter output = new StreamWriter("..\\..\\..\\ValueStatistics.csv");
-            String line, currentDataElement, value, database, recordType, submissionType;
+            String line, currentDataElement, value, database, recordType, submissionType, term;
             Dictionary<Tuple<String, String, String, String, String>, float> valuePercentageMap =
                 new Dictionary<Tuple<string, string, string, string, string>, float>();
             Dictionary<Tuple<String, String, String, String, String>, int> valueCountMap = 
                 new Dictionary<Tuple<string, string, string, string, string>, int>();
             Dictionary<String, List<String>> valueLists = new Dictionary<string, List<string>>();
+            Dictionary<Tuple<int, String, String, String>, float> valueStdDevMap = 
+                new Dictionary<Tuple<int, string, string, string>, float>();
+            Dictionary<Tuple<int, String, String, String>, float> valueMeanMap =
+                new Dictionary<Tuple<int, string, string, string>, float>();
             SqlCommand comm;
+            String[] columns;
+            float percentage;
+            int count;
 
             try
             {
@@ -37,10 +44,7 @@ namespace DataProfiler
             while (!file.EndOfStream)
             {
                 line = file.ReadLine();
-                String[] columns = line.Split(new char[]{','});
-                String term;
-                float percentage;
-                int count;
+                columns = line.Split(new char[]{','});
                 database = columns[0];
                 recordType = columns[1];
                 currentDataElement = columns[2];
@@ -90,6 +94,8 @@ namespace DataProfiler
             Tuple<String, String, String, String, String>[] keys = new Tuple<string, string, string, string, string>[valueCountMap.Keys.Count];
             valueCountMap.Keys.CopyTo(keys, 0);
 
+            //output.WriteLine("Database,Term,Submission Type,Data Element,Value,Percentage of Submission,Count");
+
             foreach (Tuple<String,String,String,String,String> key in keys)
             {
                 output.WriteLine(key.Item1 + "," + key.Item2 + "," + key.Item3 + "," + key.Item4 + "," + key.Item5 + ","
@@ -97,6 +103,85 @@ namespace DataProfiler
             }
 
             output.Close();
+
+            file = new StreamReader("..\\..\\..\\ValueStatistics.csv");
+            output = new StreamWriter("..\\..\\..\\AggregateStatistics.csv");
+                   
+            List<float> summerPercentages = new List<float>();
+            List<float> fallBOTPercentages = new List<float>();
+            List<float> fallEOTPercentages = new List<float>();
+            List<float> springBOTPercentages = new List<float>();
+            List<float> springEOTPercentages = new List<float>();
+
+            String dataElement;
+            String curValue = null;
+
+            while (!file.EndOfStream)
+            {
+                line = file.ReadLine();
+                columns = line.Split(new char[] { ',' });
+                currentDataElement = columns[3];
+                submissionType = columns[2];
+                term = columns[1];
+                value = columns[4];
+                percentage = float.Parse(columns[5]);
+
+                if (curValue != null && curValue != value)
+                {
+                    if (summerPercentages.Count != 0)
+                        output.WriteLine(currentDataElement + "," + curValue + ",1,E," + summerPercentages.Average() + "," + stdDev(summerPercentages));
+                    if (fallBOTPercentages.Count != 0)
+                        output.WriteLine(currentDataElement + "," + curValue + ",2,B," + fallBOTPercentages.Average() + "," + stdDev(fallBOTPercentages));
+                    if (fallEOTPercentages.Count != 0)
+                        output.WriteLine(currentDataElement + "," + curValue + ",2,E," + fallEOTPercentages.Average() + "," + stdDev(fallEOTPercentages));
+                    if (springBOTPercentages.Count != 0)
+                        output.WriteLine(currentDataElement + "," + curValue + ",3,B," + springBOTPercentages.Average() + "," + stdDev(fallBOTPercentages));
+                    if (springEOTPercentages.Count != 0)
+                        output.WriteLine(currentDataElement + "," + curValue + ",3,E," + springEOTPercentages.Average() + "," + stdDev(springEOTPercentages));
+
+                    summerPercentages.Clear();
+                    fallBOTPercentages.Clear();
+                    fallEOTPercentages.Clear();
+                    springBOTPercentages.Clear();
+                    springEOTPercentages.Clear();
+                }
+
+                dataElement = currentDataElement;
+                curValue = value;
+
+                if (term[0] == '1')
+                {
+                    summerPercentages.Add(percentage);
+                }
+                else if (term[0] == '2' && submissionType == "E")
+                {
+                    fallEOTPercentages.Add(percentage);
+                }
+                else if (term[0] == '2' && submissionType == "B")
+                {
+                    fallBOTPercentages.Add(percentage);
+                }
+                else if (term[0] == '3' && submissionType == "E")
+                {
+                    springEOTPercentages.Add(percentage);
+                }
+                else
+                {
+                    springBOTPercentages.Add(percentage);
+                }
+            }
+
+            file.Close();
+            output.Close();
+        }
+
+        static double stdDev(List<float> values)
+        {
+            double average = values.Average();
+
+            double sumOfSquaredOfDifferences = values.Select(val => (val - average) * (val - average)).Sum();
+
+            return Math.Sqrt(sumOfSquaredOfDifferences / values.Count);
         }
     }
 }
